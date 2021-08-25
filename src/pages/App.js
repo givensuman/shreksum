6d75c0a3901f471493476dd04188ae14c8b8fedf
Given Suman <givensuman@gmail.com>
2021-08-25 16:35:40 -0400

    Second commit

--- src/pages/App.js
+++ src/pages/App.js
@@ -6,6 +6,7 @@ import Body from '../components/Body'
 import '../global.scss'
 
 import logo from '../images/logo.png'
+import github from '../images/github.png'
 
 const App = () => {
     const [showBody, setShowBody] = useState(false)
@@ -13,6 +14,9 @@ const App = () => {
 
     return (
         <div id='app'>
+            <div id='github'>
+                <img onClick={() => window.open('https://github.com/givensuman/shreksum/')}src={github} alt='github' />
+            </div>
             <div id='shrek-wrapper'>
                 <img src={logo} alt='logo'/>
             </div>
