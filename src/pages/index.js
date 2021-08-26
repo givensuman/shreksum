e32fdf96c2b477afe1589e15dc2915949158aeb3
Given Suman <givensuman@gmail.com>
2021-08-26 10:59:18 -0400

    Fix react-helmet implementation

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,17 @@ import { Helmet } from "react-helmet"
 
 const IndexPage = () => {
   return (
-    <Helmet>
-      <html lang='en' />
-      <title>Lorem Shreksum</title>
-      <meta name='desciprtion' content="The world's only Shrek-themed Lorem Ipsum generator." />
+    <div>
+      <Helmet>
+        <html lang='en' />
+        <title>Lorem Shreksum</title>
+        <meta name='description' content="The world's only Shrek-themed Lorem Ipsum generator." />
+        <link rel="icon" 
+          type="image/png" 
+          href="../images/favicon.ico"></link>
+      </Helmet>
       <App />
-    </Helmet>
+    </div>
   )
 }
 
